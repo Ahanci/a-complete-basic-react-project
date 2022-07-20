64b88f3862245aa5818ff1ecbb53cec4c132c177
Abdulkerim Hancıoglu <ahancimerve@gmail.com>
2022-07-20 18:36:31 +0300

    reUsableButtonComponentAdded

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from './AddUser.module.css';
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 const AddUser = props => {
 
@@ -14,7 +15,7 @@ const AddUser = props => {
                 <input type='text' id="username" placeholder=" please type user name" />
                 <label htmlFor="age">Age (Years)</label>
                 <input type='number' id="age" placeholder=" please type your age" />
-                <button type="submit" >Add User</button>
+                <Button type="submit" >Add User</Button>
             </form>
 
         </Card>
