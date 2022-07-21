93647b046a0447838dba0a03c151807fac65588c
Abdulkerim Hancıoglu <ahancimerve@gmail.com>
2022-07-21 07:51:20 +0300

    errorModalAdded

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,59 +1,64 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from './AddUser.module.css';
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = props => {
-    const [enteredUserName, setEnteredUserName]= useState('');
-    const [enteredAge, setEnteredAge]= useState('');
+    const [enteredUserName, setEnteredUserName] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
 
     const addUserHandler = (e) => {
         e.preventDefault();
-        if(enteredUserName.trim().length === 0 || enteredAge.trim().length === 0){
+        if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
             return;
         }
-        if(+enteredAge < 1){
+        if (+enteredAge < 1) {
             return;
         }
-       props.onAddUser(enteredUserName, enteredAge);       
+        props.onAddUser(enteredUserName, enteredAge);
 
         setEnteredAge('');
         setEnteredUserName('');
     }
 
-    const userNameChangeHandler = e=>{
+    const userNameChangeHandler = e => {
         setEnteredUserName(e.target.value)
 
     }
 
-    const ageChangeHandler = e=>{
+    const ageChangeHandler = e => {
         setEnteredAge(e.target.value)
-        
+
     }
     return (
-        <Card className={styles.input} >
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">username</label>
-                <input 
-                type='text' 
-                id="username" 
-                placeholder=" please type user name" 
-                onChange={userNameChangeHandler}
-                value={enteredUserName}/>
+        <div>
+            <ErrorModal title='an error occurred' message='hoppps' />
+            <Card className={styles.input} >
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">username</label>
+                    <input
+                        type='text'
+                        id="username"
+                        placeholder=" please type user name"
+                        onChange={userNameChangeHandler}
+                        value={enteredUserName} />
 
-                <label htmlFor="age">Age (Years)</label>
-                <input 
-                type='number' 
-                id="age" 
-                placeholder=" please type your age" 
-                onChange={ageChangeHandler}
-                value={enteredAge}
-                
-                />
-                <Button type="submit" >Add User</Button>
-            </form>
+                    <label htmlFor="age">Age (Years)</label>
+                    <input
+                        type='number'
+                        id="age"
+                        placeholder=" please type your age"
+                        onChange={ageChangeHandler}
+                        value={enteredAge}
 
-        </Card>
+                    />
+                    <Button type="submit" >Add User</Button>
+                </form>
+
+            </Card>
+        </div>
+
 
     )
 };
